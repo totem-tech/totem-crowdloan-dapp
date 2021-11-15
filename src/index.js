cc7f517e62645de4e31fe91c36d23249e3da456f
Toufiqur Chowdhury <htr.letun@gmail.com>
2021-11-16 00:28:35 +0300

    Added coming soon text and some links

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => <div>
-    This is react app with webpack
-</div>
+const ComingSoon = () => (
+    <div style={{ textAlign: 'center' }}>
+        <br />
+        <h1>Totem Crowdloan is coming soon.</h1>
+        <h3>Stay tuned!</h3>
+        <p>Check out our <a href="https://totem.live">testnet app</a>.</p>
+        <p>Join us on <a href="https://discord.gg/Vx7qbgn">Discord</a> and <a href="https://t.me/totemchat">Telegram</a></p>
+        <p>For more information visit <a href="https://totemaccounting.com">our website.</a></p>
+    </div>
+)
 ReactDOM.render(
-    <App />,
+    <ComingSoon />,
     document.getElementById('root'),
-);
-
-
-const test = () => {
-    require('web3-utils')
-}
+)
